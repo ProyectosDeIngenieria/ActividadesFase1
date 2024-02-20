91c752b18812425db27d1692c47b6f496aadc19d
Manuel Martínez <manuel.mtzv816@gmail.com>
2024-02-20 14:00:44 -0600

    feat: keep track of coincidences with a dict to save word metadata

--- src/main.py
+++ src/main.py
@@ -1,7 +1,10 @@
+from collections import defaultdict
 import os
 import re
 import time
 import sys
+
+SUMMARY_FILE = 'summary.txt'
 
 def get_html_file_text(file_path: str) -> str:
     """Gets the content of an html file without the tags
@@ -32,7 +35,7 @@ def order_words(file_path: str) -> list:
     """
     text_content = get_html_file_text(file_path)
     # Split the content into words
-    words = text_content.split()
+    words = re.findall(r'\b(?![0-9]+\b)\w+(?:-\w+)*\b', text_content.lower());
     # Order the words alphabetically
     words.sort()
     return words
@@ -42,31 +45,39 @@ def validate_paths(input_directory: str, output_directory: str):
         raise Exception('La carpeta de entrada no existe')
     if not os.path.exists(output_directory):
         print('La carpeta de salida no existe, se creará una nueva')
-        os.makedirs(output_directory)   
+        os.makedirs(output_directory)    
+
+def append_word_to_summary(word_summary: dict, word: str):
+    word_summary[word]['coincidences'] += 1
 
 def main(input_directory, output_directory):
     validate_paths(input_directory, output_directory)
-    
+
     process_start_time = time.time()
     # Generate folder path
     main_path = os.getcwd()
-    html_path = os.path.join(main_path, 'html')
-    result_path = os.path.join(main_path, 'results')
-    alphabetically = os.path.join(main_path, 'alphabetically')
-    # Ensure 'alphabetically' directory exists
-    if not os.path.exists(alphabetically):
-        os.makedirs(alphabetically)
+    input_path = os.path.join(main_path, input_directory)
+    output_path = os.path.join(main_path, output_directory)
     # Get files names
-    files = sorted(os.listdir(html_path))
+    files = sorted(os.listdir(input_path))
     # For each file create a new one with the words ordered alphabetically
     combined_words = []
     total_time = 0
+
+    word_summary = defaultdict(lambda: {'coincidences': 0}) # Hint: this structure could help in the next activity (Activity 6)
+
     for file in files:
         try:
             # Calculate time to process file
             start_time = time.time()
-            words_file = order_words(os.path.join(html_path, file))
-            combined_words.extend(words_file)
+            words_file = order_words(os.path.join(input_path, file))
+            combined_words.extend(words_file)            
+
+            with open(os.path.join(output_path, re.sub(r'\.[^.]+$', '.txt', file)), 'w', encoding='utf-8') as file_result:
+                for word in words_file:
+                    file_result.write(f"{word}\n")           
+                    append_word_to_summary(word_summary, word)    
+
         except Exception as e:
             print(f"Error processing {file}: {e}")
         end_time = time.time()
@@ -74,9 +85,10 @@ def main(input_directory, output_directory):
         total_time += execution_time
 
     # Write combined words to a single file
-    combined_file_path = os.path.join(alphabetically, 'combined.txt')
+    combined_file_path = os.path.join(output_path, SUMMARY_FILE)
     with open(combined_file_path, 'w', encoding='utf-8') as combined_file:
-        combined_file.write('\n'.join(combined_words))
+        for word, summary in word_summary.items():
+            combined_file.write(f"{word}; {summary['coincidences']}\n")
 
     # Write the total processing time
     with open(combined_file_path, 'a', encoding='utf-8') as combined_file:
@@ -84,7 +96,7 @@ def main(input_directory, output_directory):
         process_end_time = time.time()
         combined_file.write(f"\n\nTotal execution time: {(process_end_time - process_start_time):.20f} seconds")
 
-    print("Proceso completado. Se ha creado el archivo combined.txt en la carpeta 'alphabetically'.")
+    print(f"Proceso completado. Se ha creado el archivo {SUMMARY_FILE} en la carpeta {output_path}.")
 
 
 if __name__ == '__main__':
